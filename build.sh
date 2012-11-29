8d93223e3b2a04d9fa332587590088bd1b49e5f8
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2012-11-29 13:30:42 +0100

    running tests from a script

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 ln -sf `pwd`/phantomjs/macosx/phantomjs-1.7.0-macosx/bin/phantomjs /usr/local/bin/phantomjs
 ln -sf `pwd`/casperjs/bin/casperjs /usr/local/bin/casperjs
+
+cd test;
+casperjs ap_main_page.js
