38d20f972fb3279a0866a997fe6c710644045589
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2012-12-07 16:09:21 +0100

    experimenting with linux build script

--- buildLinux.sh
+++ buildLinux.sh
@@ -3,8 +3,8 @@ export CASPERJS_HOME=`pwd`/casperjs/bin/
 
 PATH="$PATH":$PHANTOMJS_HOME:$CASPERJS_HOME
 
-echo 'Phantom version: ' `phantomjs --version`;
-echo 'Casper version:  ' `casperjs --version`;
+phantomjs --version;
+casperjs --version;
 
 cd test;
 casperjs test ap_main_page.js --url=$1;
